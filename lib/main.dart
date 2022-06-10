aac73e040216813d80090527f0ff15783c8fdbf1
MArslan88 <gery_maz_555@hotmail.com>
2022-06-10 18:35:17 +0500

    GetAllCategories implemented

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:login_form_flutter/models/all_categories_model.dart';
 import 'package:login_form_flutter/models/login_response.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Login(),
     );
   }
@@ -29,6 +31,7 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   UserData? userData;
+  List<AllCategoriesData>? categories;
 
   //for loading animation
   bool isLoading = false;
@@ -49,11 +52,21 @@ class _LoginState extends State<Login> {
               children: [
                 if (error != null) Text("There is an error : $error"),
                 if (userData == null) ...[
+                  // this is 'Spread' operator it will combine it with above mentioned condition
                   TextField(controller: userNameCtrl),
                   TextField(controller: passwordCtrl),
                   ElevatedButton(onPressed: login, child: Text("Login"))
                 ],
-                if (userData != null) Text(userData?.accessToken ?? "")
+                if (userData != null) Text(userData?.accessToken ?? ""),
+                if (categories != null)
+                  Expanded(
+                      child: ListView.builder(
+                    itemCount: categories!.length,
+                    itemBuilder: (_, index) => ListTile(
+                      title: Text(categories![index].title ?? ""),
+                      leading: Image.network(categories![index].icon ?? ""),
+                    ),
+                  ))
               ],
             )
           : Center(child: CircularProgressIndicator()),
@@ -71,6 +84,31 @@ class _LoginState extends State<Login> {
       var responseJSON = LoginResponse.fromJson(jsonDecode(response.body));
       setState(() {
         userData = responseJSON.data;
+      });
+      await getAllCategories(); // Categories method call
+      // here await will stop this 'try' until categories not get
+    } catch (e) {
+      setState(() {
+        error = e.toString();
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> getAllCategories() async {
+    var url = Uri.parse('http://ishaqhassan.com:2000/category');
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var response = await http.get(url,
+          headers: {"Authorization": "Bearer ${userData?.accessToken}"});
+      var responseJSON =
+          AllCategoriesResponse.fromJson(jsonDecode(response.body));
+      setState(() {
+        categories = responseJSON.data;
       });
     } catch (e) {
       setState(() {
